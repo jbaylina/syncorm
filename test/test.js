8c674c35eac3b2e6671f1abd933c539502e80377
Jordi Baylina <baylina@uniclau.com>
2015-03-09 14:54:17 +0100

    Start to develop mocha tests

--- test/test.js
+++ test/test.js
@@ -25,17 +25,18 @@ function createTestDatabase(done) {
     this.timeout(10000);
     var connection = mysql.createConnection({
         host: "127.0.0.1",
+        timezone: "utc",
         user: "root"
     });
-    async.series([function() {
-        connection.connect();
-    }, function() {
-        connection.query('DROP DATABASE IF EXISTS `syncorm_test`');
-    }, function() {
-        connection.query('CREATE SCHEMA `syncorm_test` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci');
-    }, function() {
-        connection.query('USE syncorm_test');
-    }, function() {
+    async.series([function(cb) {
+        connection.connect(cb);
+    }, function(cb) {
+        connection.query('DROP DATABASE IF EXISTS `syncorm_test`', cb);
+    }, function(cb) {
+        connection.query('CREATE SCHEMA `syncorm_test` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci', cb);
+    }, function(cb) {
+        connection.query('USE syncorm_test', cb);
+    }, function(cb) {
         connection.query(
             'CREATE  TABLE `syncorm_test`.`examples1` (' +
               '`id` INT NOT NULL ,' +
@@ -46,12 +47,12 @@ function createTestDatabase(done) {
               '`double` DOUBLE NULL ,' +
               '`decimal112` DECIMAL(11,2) NULL ,' +
               '`boolean` TINYINT NULL ,' +
-              'PRIMARY KEY (`id`) )');
-    }, function() {
+              'PRIMARY KEY (`id`) )', cb);
+    }, function(cb) {
         connection.query(
             " INSERT INTO `syncorm_test`.`examples1` " +
             "        (`id`,`string8` ,`text`  ,`date`      ,`datetime`               ,`double`, `decimal112`  ,`boolean`)" +
-            " VALUES ('1' ,'a string','a text','2015-03-09','2015-04-09 13:14:15.123','3.1416', '123456789.01','1'      )");
+            " VALUES ('1' ,'a string','a text','2015-03-09','2015-04-09 13:14:15','3.1416', '123456789.01','1'      )", cb);
     }], done);
 
 }
@@ -89,28 +90,28 @@ describe('Sync orm test', function() {
             db.loadAll();
         });
         it("should have one object", function() {
-            assert(_.size(db.examples1) === 1);
+            assert.equal(_.size(db.examples1), 1);
         });
         it("id should be 1", function() {
-            assert(db.examples1[1].id === 1);
+            assert.equal(db.examples1[1].id, 1);
         });
         it("string8 should be 'a string'", function() {
-            assert(db.examples1[1].string8 === 'a string');
+            assert.equal(db.examples1[1].string8,'a string');
         });
-        it("text should be a test", function() {
-            assert(db.examples1[1].test === 'a text');
+        it("text should be a text", function() {
+            assert.equal(db.examples1[1].text, 'a text');
         });
         it("date should be a specific date", function() {
-            assert(db.examples1[1].date.toIsoString() === "2015-03-09T00:00:00.000Z");
+            assert.equal(db.examples1[1].date.toISOString(), "2015-03-09T00:00:00.000Z");
         });
         it("date should be a specific datetime", function() {
-            assert(db.examples1[1].datetime.toIsoString() === "2015-04-09T13:14:15.123Z");
+            assert.equal(db.examples1[1].datetime.toISOString(), "2015-04-09T13:14:15.000Z");
         });
         it("double should be 3.1416", function() {
-            assert(db.examples1[1].double === 3.1416);
+            assert.equal(db.examples1[1].double, 3.1416);
         });
         it("decimal should be init value", function() {
-            assert(db.examples1[1].boolean === true);
+            assert.equal(db.examples1[1].boolean, true);
         });
     });
 });
